159a74f9528acea8b1f2887b9695820d3ca61e15
Colin Bose <colin.bose2@gmail.com>
2016-01-26 17:01:41 -0800

    Colin Pushing tutorial 2

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class About extends CI_Controller {
+class About extends Application {
 
 	/**
 	 * Index Page for this controller.
@@ -19,7 +19,8 @@ class About extends CI_Controller {
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
-	{
-		$this->load->view('about');
+	{	//$this->load->view('welcome');
+            $this->data['pagebody'] = 'about';
+            $this->render();
 	}
 }
